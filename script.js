adb8db79da680cbc6188b38fd96c160f6068269e
marnason <pmarnason@gmail.com>
2025-03-24 01:40:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ c.height = window.innerHeight;
 c.width = window.innerWidth;
 
 // the characters
-var konkani  = "゠アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレワヰヱヲンヺ・ーヽヿ0123456789"
+var konkani  = "日ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍｦｲｸｺｿﾁﾄﾉﾌﾔﾖﾙﾚﾛﾝ:・.\"=*+-<>¦｜çﾘｸ二コソヤ0123456789"
+// var konkani  = "゠アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレワヰヱヲンヺ・ーヽヿ0123456789"
 // converting the string into an array of single characters
 var characters = konkani.split("");
 var font_size = 14;
